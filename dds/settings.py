c9d8ad58ad2b584da17133378f53c653b6eab2b4
EC2 Default User <ec2-user@ip-172-31-42-230.eu-north-1.compute.internal>
2025-05-02 21:43:12 +0000

    Cleaned up .pyc files and updated settings/static files

--- dds/settings.py
+++ dds/settings.py
@@ -149,13 +149,12 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = '/var/www/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
