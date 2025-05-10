6965c5a4b3694d67051b4e8f16587a705aeb4326
Kevin <kevinolm10@gmail.com>
2025-05-10 18:05:01 +0200

    Move all post view here

--- home/views.py
+++ home/views.py
@@ -1,8 +1,11 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
-
-from blog.models import Post  # Use capital P for consistency
-
+from blog.models import Post
 
 def home(request):
-    posts = Post.objects.all()
+    post_list = Post.objects.all().order_by('-created_at')  # Order posts by creation date
+    paginator = Paginator(post_list, 6)  # Show 6 posts per page
+    page_number = request.GET.get('page')  # Get the current page number from the URL
+    posts = paginator.get_page(page_number)  # Get the paginated posts for the current page
+
     return render(request, 'home/home.html', {'posts': posts})
